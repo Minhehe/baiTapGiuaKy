b50a90fc852c62d21a27d822fde8c117f9782ad2
Minhehe <163635856+Minhehe@users.noreply.github.com>
2024-12-11 16:47:03 +0700

    delete.php

--- delete.php
+++ delete.php
@@ -10,15 +10,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$id = $_GET['id']; // Lấy id sinh viên từ URL
+$id = $_GET['id']; // Lấy id sinh viên 
 
-// Xóa sinh viên
+//----------------------- xoá sv -----------------------------------//
 $sql = "DELETE FROM table_Students WHERE id = $id";
 
 if ($conn->query($sql) === TRUE) {
     echo "Xóa sinh viên thành công!";
     $conn->query("ALTER TABLE table_Students AUTO_INCREMENT = 1");
-    header("Location: index.php"); // Quay lại trang danh sách
+    header("Location: index.php"); // return trang chủ
 } else {
     echo "Lỗi: " . $sql . "<br>" . $conn->error;
 }
